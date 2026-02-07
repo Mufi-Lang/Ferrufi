df57ce6f40e8850c5ee639c1e296db056de5380d
Mustafif <mustafif0929@gmail.com>
2026-02-07 18:47:05 -0500

    docs: Set website favicon to Ferrufi logo

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -7,7 +7,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 const config: Config = {
   title: 'Ferrufi',
   tagline: 'High-performance macOS IDE for the Mufi programming language',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/logo.png',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
